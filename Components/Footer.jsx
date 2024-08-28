34a44be5c59d2f0c57130b095e3a5d783e73888a
ashok07-ai <ak850860@gmail.com>
2024-08-29 00:59:15 +0545

    header and footer completed

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,102 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const footerList = ["Cryptocash", "How it works", "Token", "FAQ", "Contact"];
+  return (
+    <footer>
+      <div
+        className="top_footer bg_light_dark"
+        data-z-index="1"
+        data-parallax="scroll"
+        data-image-src="assets/images/footer_bg.png"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-12">
+              <div
+                className="footer_logo mb-3 animation"
+                data-animation="fadeInUp"
+                data-animation-delay="0.2s"
+              >
+                <a href="#home_section" className="page-scroll">
+                  <img src="assets/images/footer_logo.png" alt="" />
+                </a>
+              </div>
+
+              <p
+                className="animation text-light"
+                data-animation="fadeInUp"
+                data-animation-delay="0.4s"
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Accusamus architecto voluptatum odit, vero facilis nulla quis
+                molestias! Mollitia eius voluptate dolores? Illum vitae ipsam
+                vero nisi ratione neque ipsum? Rem!
+              </p>
+            </div>
+            <div className="col-lg-3 col-md-6 res_md_mt_30 res_sm_mt_20">
+            <h4
+              className="footer_title border_title animation"
+              data-animation="fadeInUp"
+              data-animation-delay="0.2s"
+            >
+              Quick Links
+            </h4>
+            <ul className="footer_link">
+              {footerList.map((list, i) => (
+                <li
+                  key={i}
+                  className="animation"
+                  data-animation="fadeInUp"
+                  data-animation-delay={`0.${i + 2}s`}
+                >
+                  <a href="#">{list}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="col-lg-5 col-md-6 res_md_mt_30 res_sm_mt_20">
+            <div className="newsletter_form">
+              <h4 className="footer_title border_title animation">
+                NewsLetter
+              </h4>
+              <p
+                className="animation"
+                data-animation="fadeInUp"
+                data-animation-delay="0.4s"
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Accusamus architecto voluptatum odit, vero facilis nulla quis
+                molestias! Mollitia eius voluptate dolores? Illum vitae ipsam
+                vero nisi ratione neque ipsum? Rem!
+              </p>
+
+              <form action="#" className="subscribe_form animation" data-animation="fadeInUp" data-animation-delay="0.4s">
+                <input type="text" required placeholder="Enter email" className="input-rounded" />
+
+                <button type="submit" title="Subscribe" className="btn-info" name="submit" value="Submit">Subscribe</button>
+              </form>
+            </div>
+          </div>
+          </div>
+
+          
+        </div>
+      </div>
+      <div className="bottom_footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="copyright">
+                Copyright &copy; 2023 All Rights Reserved
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {BiMenu} from  "react-icons/bi"
+import { BiMenu } from "react-icons/bi";
 
-const Header = ({address, setAddress, connectWallet}) => {
+const Header = ({ address, setAddress, connectWallet }) => {
   const menuList = [
     {
       menu: "Home",
@@ -32,7 +32,7 @@ const Header = ({address, setAddress, connectWallet}) => {
       link: "#contact",
     },
   ];
-  console.log(menuList)
+  console.log(menuList);
   return (
     <div className="header_wrap fixed-top">
       <div className="container-fluid">
@@ -70,37 +70,44 @@ const Header = ({address, setAddress, connectWallet}) => {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav m-auto">
-              {
-                
-                menuList.map((menu, i)=>{
-                  <li key={i+1} className="animation" data-animation="fadeInDown" data-animation-delay={`1.${i+1}s`}>
-                    <a href={menu.link} className="nav-link">
-                      {menu.menu}
-                    </a>
-                  </li>
-                })
-              }
+              {menuList.map((menu, i) => (
+                <li
+                  key={i + 1}
+                  className="animation"
+                  data-animation="fadeInDown"
+                  data-animation-delay={`1.${i + 1}s`}
+                >
+                  <a href={menu.link} className="nav-link">
+                    {menu.menu}
+                  </a>
+                </li>
+              ))}
             </ul>
 
             <ul className="navbar-nav nav_btn align-items-center">
-              <li className="animation" data-animation="fadeInDown" data-animation-delay="1s">
-                {
-                  address ? (
-                    <a className="btn btn-default btn-radius nav_item">
-                      <small className="new-color">
-                        {""}
-                        {address.slice(0,5)}...{address.slice(-5)}
-                      </small>
-                    </a>
-                  ) : (
-                    <a onClick={() => connectWallet()} className="btn btn-default btn-radius nav_item">
+              <li
+                className="animation"
+                data-animation="fadeInDown"
+                data-animation-delay="1s"
+              >
+                {address ? (
+                  <a className="btn btn-default btn-radius nav_item">
+                    <small className="new-color">
+                      {""}
+                      {address.slice(0, 5)}...{address.slice(-5)}
+                    </small>
+                  </a>
+                ) : (
+                  <a
+                    onClick={() => connectWallet()}
+                    className="btn btn-default btn-radius nav_item"
+                  >
                     <small className="new-color">
                       {""}
                       Connect
                     </small>
                   </a>
-                  )
-                }
+                )}
               </li>
             </ul>
           </div>
